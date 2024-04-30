a8c8169081f4d82191113d1cbdc84559c2b517d0
soubhikdas <d.soubhik@hotmail.com>
2024-05-01 01:32:50 +0530

    app update

---
+++ app/audioplayer/datascript.js
@@ -0,0 +1,70 @@
+"use strict";
+const urlParams = new URLSearchParams(window.location.search);
+const tracksrc = urlParams.get("audio_url");
+const tracktitle = urlParams.get("title");
+const trackartist = urlParams.get("artist");
+const trackthumb = urlParams.get("thumb");
+const player = document.getElementById("audioplayer");
+const title = document.getElementById("trtt");
+const artist = document.getElementById("trat");
+const thumb = document.getElementById("trth");
+player.setAttribute("data-src", tracksrc);
+title.innerText = tracktitle;
+artist.innerText = trackartist;
+thumb.src = trackthumb;
+console.log(tracksrc, tracktitle, trackartist);
+if (!tracksrc) {
+  document.body.style.backgroundImage = "url('player-thumb.png')";
+  document.getElementById("trckdesc").style.display = "none";
+}
+document.addEventListener("contextmenu", function (event) {
+  event.preventDefault();
+});
+
+document
+  .getElementById("audioForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var audioUrl = document.getElementById("adurl").value;
+    var thumbnailUrl = document.getElementById("adthumb").value;
+    var title = document.getElementById("adtit").value;
+    var artist = document.getElementById("adart").value;
+
+    var baseUrl = "https://soubhikdas.in/app/audioplayer/";
+    var url =
+      baseUrl +
+      "?audio_url=" +
+      encodeURIComponent(audioUrl) +
+      "&thumb=" +
+      encodeURIComponent(thumbnailUrl) +
+      "&title=" +
+      encodeURIComponent(title) +
+      "&artist=" +
+      encodeURIComponent(artist);
+
+    window.location.href = url;
+  });
+
+function GetCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return null;
+}
+var useru = GetCookie("_dfunc");
+var crtaudlnk = document.getElementById("crtaudlnk");
+if (crtaudlnk && useru != null) {
+  crtaudlnk.addEventListener("click", function () {
+    document.getElementById("crtlnkdv").style.display = "block";
+  });
+}
