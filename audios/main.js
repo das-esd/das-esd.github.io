2ac10f1c5814ca37ea2851f7eec8bf5c5edf9666
soubhikdas <d.soubhik@hotmail.com>
2024-05-24 22:38:17 +0530

    new recording update

--- audios/main.js
+++ audios/main.js
@@ -22,6 +22,12 @@ let repeat_btn = document.querySelector(".repeat-track");
 // Define the tracks that have to be played
 let track_list = [
   {
+    name: "Inside Mirror",
+    artist: "Es D (Soubhik Das)",
+    image: "/audios/files/image/record_thumb.png",
+    path: "/audios/files/audio/record/Inside mirror.m4a",
+  },
+  {
     name: "মহাবীর (Great Warrior)",
     artist: "Es D (Soubhik Das)",
     image: "/audios/files/image/record_thumb.png",
