515281a727014e92ce1c83067c3c11a24b590d7a
soubhikdas <d.soubhik@hotmail.com>
2024-06-22 01:02:31 +0530

    new dsmusic update

--- js/scripts.js
+++ js/scripts.js
@@ -475,7 +475,8 @@ function createCookie(name, value, days) {
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     expires = "; expires=" + date.toUTCString();
   }
-  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  document.cookie =
+    name + "=" + (value || "") + expires + "; path=/;domain=soubhikdas.in";
 }
 
 async function validateForm(event) {
