18836bcacc0ef659cde6ccbd32ac4880ffb43cf7
soubhikdas <d.soubhik@hotmail.com>
2024-07-20 06:28:37 +0530

    Update main.js

--- audios/main.js
+++ audios/main.js
@@ -22,6 +22,12 @@ let repeat_btn = document.querySelector(".repeat-track");
 // Define the tracks that have to be played
 let track_list = [
   {
+    name: "Moonlight Melancholy",
+    artist: "Es D (Soubhik Das)",
+    image: "/audios/files/image/record_thumb.png",
+    path: "/audios/files/audio/record/Moonlight Melancholy.m4a",
+  },
+  {
     name: "Lord একা",
     artist: "Es D (Soubhik Das)",
     image: "/audios/files/image/record_thumb.png",
