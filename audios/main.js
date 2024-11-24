4252a05320a701e4f51d9ea12127d6474545c5c1
soubhikdas <d.soubhik@hotmail.com>
2024-11-24 23:01:17 +0530

    record and sonic muse update

--- audios/main.js
+++ audios/main.js
@@ -22,6 +22,12 @@ let repeat_btn = document.querySelector(".repeat-track");
 // Define the tracks that have to be played
 let track_list = [
   {
+    name: "Divider",
+    artist: "Es D (Soubhik Das)",
+    image: "/audios/files/image/record_thumb.png",
+    path: "/audios/files/audio/record/Divider.m4a",
+  },
+  {
     name: "মানুষ (Human)",
     artist: "Es D (Soubhik Das)",
     image: "/audios/files/image/record_thumb.png",
