2b6e10c0ad539bd2c172d1d6b1e974fa15d6e76f
soubhikdas <d.soubhik@hotmail.com>
2025-01-15 22:36:36 +0530

    up up date

--- audios/main.js
+++ audios/main.js
@@ -22,6 +22,12 @@ let repeat_btn = document.querySelector(".repeat-track");
 // Define the tracks that have to be played
 let track_list = [
   {
+    name: "Blue Sky",
+    artist: "Es D (Soubhik Das)",
+    image: "/audios/files/image/record_thumb.png",
+    path: "/audios/files/audio/record/Blue Sky.m4a",
+  },
+  {
     name: "বলো হরি",
     artist: "Es D (Soubhik Das)",
     image: "/audios/files/image/record_thumb.png",
